a17924dc9d91cfff5befe694dd894bfd1d0fd724
LiorRavid <simoplay@gmail.com>
2021-10-27 14:00:10 +0300

    add canvas and image drawing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 'use strict'
+
+var gElCanvas
+var gCtx
+
+function init() {
+  gElCanvas = document.querySelector('canvas')
+  console.log('gElCanvas',gElCanvas);
+  gCtx = gElCanvas.getContext('2d')
+  drawImg()
+  // resizeCanvas()
+}
+
+function drawImg() {
+    var img = new Image();
+    img.src= getImgUrl(getSelectedImgId())
+    // add an imageId instead of 5
+    img.onload = () => {
+      gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+    };
+  }
+  
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -4,11 +4,11 @@ const KEY_GMEME = 'gMemeData'
 
 var gKeywords = {'happy': 12,'funny puk': 1}
 
-var gId = 0
+var gIdx = 0
 
 var gImgs = _createImgs()
 
-var gMeme = createMeme(5,0)
+var gMeme = createMeme(7,0)
 
 function createMeme(selectedImgId, selectedLineIdx){
     var meme = {
@@ -41,7 +41,7 @@ function _createImgs(){
     var imgs = []
     for(var i=0;i<18;i++){
         imgs.push(_createImg())
-        gId++
+        gIdx++
     }
     console.log('imgs',imgs)
     return imgs
@@ -49,12 +49,20 @@ function _createImgs(){
 
 function _createImg(){
     var img = {
-        id: gId,
-        url:`img/${gId+1}.jpg`, 
-        keywords: ['happy']
+        id: gIdx,
+        url:`img/${gIdx+1}.jpg`, 
+        keywords: []
     }
     console.log('img',img)
     return img
+}
+
+function getImgUrl(selectedImgId){
+    return gImgs[selectedImgId].url
+}
+
+function getSelectedImgId(){
+    return gMeme.selectedImgId
 }
 
 
@@ -62,6 +70,11 @@ function _createImg(){
 // TODO
 // function addImg
 // function getImgs
+// createKeyWord
+// createKeyWords
+// getKeyWord
+// getImgURL dynamic
+// לעשות השמה של מילות מפתח
 
 
 
