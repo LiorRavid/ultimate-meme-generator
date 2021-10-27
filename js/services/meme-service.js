c14cdc77945cb61772a5894e02691284548c0ea4
LiorRavid <simoplay@gmail.com>
2021-10-27 12:31:48 +0300

    add create imgs

--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -4,7 +4,9 @@ const KEY_GMEME = 'gMemeData'
 
 var gKeywords = {'happy': 12,'funny puk': 1}
 
-var gImgs = [{id: 1, url: 'img/popo.jpg', keywords: ['happy']}];
+var gId = 0
+
+var gImgs = _createImgs()
 
 var gMeme = createMeme(5,0)
 
@@ -14,7 +16,7 @@ function createMeme(selectedImgId, selectedLineIdx){
         selectedLineIdx,
         lines: _createLines()
     }
-    console.log('meme',meme);
+    console.log('meme',meme)
     return meme
 }
 
@@ -27,12 +29,41 @@ function _createLines(){
 function _createLine(){
     var line = {
         txt: 'I never eat Falafel', 
-            size: 20, align: 'left', 
+            size: 20, 
+            align: 'left', 
             color: 'red' 
     }
     console.log('line',line);
     return line
 }
+
+function _createImgs(){
+    var imgs = []
+    for(var i=0;i<18;i++){
+        imgs.push(_createImg())
+        gId++
+    }
+    console.log('imgs',imgs)
+    return imgs
+}  
+
+function _createImg(){
+    var img = {
+        id: gId,
+        url:`img/${gId+1}.jpg`, 
+        keywords: ['happy']
+    }
+    console.log('img',img)
+    return img
+}
+
+
+
+// TODO
+// function addImg
+// function getImgs
+
+
 
 
 // var gMeme = { 
@@ -46,3 +77,5 @@ function _createLine(){
     //         }
     //     ]
     // }
+
+    // var gImgs = [{id: 1, url: 'img/popo.jpg', keywords: ['happy']}]
